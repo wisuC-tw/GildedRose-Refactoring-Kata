8581551e1f8936b03e1ab782a7f1fb77d43b0789
Choi Wi Su <wisuchoi@Chois-MacBook-Pro.local>
2021-07-09 10:57:21 +0800

    fix unit test for sulfuras

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -38,12 +38,12 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(50, items[0].quality)
 
     def test_sulfuras_should_not_update(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 1, 40)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 1, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
         self.assertEqual(1, items[0].sell_in)
-        self.assertEqual(40, items[0].quality)
+        self.assertEqual(80, items[0].quality)
 
     def test_backstage_pass_should_increase_quality_by_2_when_sellin_less_than_or_equal_to_10(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
